bf4eb5be983ff673c5c6dec3253fce263722e45b
Rainer Poisel <rainer@honeytreelabs.com>
2024-09-28 11:23:09 +0200

    refactor: make ip address determination code more robust

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,8 @@
+import json
 from ipaddress import IPv4Address
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
+import network
 import openwrt
 
 
@@ -12,3 +14,12 @@ def test_openwrt_get_ip_addr_ok() -> None:
         ]
     ]
     assert openwrt.get_ip_addr(runner, "tun0") == IPv4Address("172.17.0.6")
+
+
+@patch("network.bash_run")
+def test_primary_host_ip_ok(mock_bash_run) -> None:
+    ip_r_s_output = json.dumps([{"dev": "eth0"}])
+    ip_a_s_output = json.dumps([{"addr_info": [{"local": "192.168.1.1"}]}])
+    mock_bash_run.side_effect = [ip_r_s_output, ip_a_s_output]
+
+    assert network.primary_host_ip() == IPv4Address("192.168.1.1")
--- util/network.py
+++ util/network.py
@@ -5,7 +5,9 @@ from process import bash_run
 
 
 def primary_host_ip() -> IPv4Address:
-    ip_cfg_str = bash_run("ip -4 -json r s")
-    ip_cfg = json.loads(ip_cfg_str)
-    primary_ip = ip_cfg[0]["prefsrc"]
-    return IPv4Address(primary_ip)
+    ip_r_s_str = bash_run("ip -4 -json r s")
+    ip_r_s = json.loads(ip_r_s_str)
+    primary_interface = ip_r_s[0]["dev"]
+    ip_a_s_str = bash_run(f"ip -4 -json -o addr show dev {primary_interface}")
+    ip_a_s = json.loads(ip_a_s_str)
+    return IPv4Address(ip_a_s[0]["addr_info"][0]["local"])
