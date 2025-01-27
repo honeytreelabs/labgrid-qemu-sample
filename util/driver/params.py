6b00536fb78f21a8283f2f73d09c12f27f7a9ded
Rainer Poisel <rainer@honeytreelabs.com>
2025-01-27 22:31:29 +0100

    chore: proper typing for QEMUParams.overwrite parameter

--- util/driver/params.py
+++ util/driver/params.py
@@ -6,4 +6,6 @@ from labgrid.resource import Resource
 @target_factory.reg_resource
 @attr.s(eq=False)
 class QEMUParams(Resource):
-    overwrite: bool = attr.ib(validator=attr.validators.instance_of(bool))
+    overwrite: bool | None = attr.ib(
+        default=False, validator=attr.validators.optional(attr.validators.instance_of(bool))
+    )
